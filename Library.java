3d5e269bbf46cbfacba291b7c141fb8975f73a0e
raja-kesharwani <rajakesharwani2628@gmail.com>
2025-05-22 19:43:39 +0530

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Library {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added successfully.");
+    }
+
+    public void displayBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books available.");
+        } else {
+            for (Book b : books) {
+                System.out.println(b);
+            }
+        }
+    }
+
+    public Book searchBookByTitle(String title) {
+        for (Book b : books) {
+            if (b.getTitle().equalsIgnoreCase(title)) {
+                return b;
+            }
+        }
+        return null;
+    }
+}
