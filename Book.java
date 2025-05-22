1ff1ea21a66abcc990b26be3652355bbd3a3b920
raja-kesharwani <rajakesharwani2628@gmail.com>
2025-05-22 19:17:59 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isIssued;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isIssued = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void issueBook() {
+        isIssued = true;
+    }
+
+    public void returnBook() {
+        isIssued = false;
+    }
+
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + isbn +
+               ", Status: " + (isIssued ? "Issued" : "Available");
+    }
+}
